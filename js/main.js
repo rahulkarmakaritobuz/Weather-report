82983d72d0217192acf60a7365ff4bfda5a3c75c
rahulkarmakaritobuz <rahul.karmakar@itobuz.com>
2023-03-02 17:40:56 +0530

    Weather V4

--- js/main.js
+++ js/main.js
@@ -1,50 +1,81 @@
-const place = document.getElementById('location');
-const actualTemp = document.getElementById('actualTemp');
-const feelsLike = document.getElementById('feelsLike');
-const weatherIcon = document.getElementById('weatherIcon');
-const setLocation = document.getElementById('setLocation');
+// const place = document.getElementById("location");
+const actualTemp = document.getElementById("actualTemp");
+const feelsLike = document.getElementById("feelsLike");
+const weatherIcon = document.getElementById("weatherIcon");
+const setLocation = document.getElementById("setLocation");
 
-var api="";
-
+let api =
+  "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=Kolkata&aqi=no";
 
 console.log(api);
 const getWeatherData = async () => {
-    console.log(api);
-    const response = await fetch(api)
-    .then((response) => {
-        return response.json();
+  console.log(api);
+  const response = await fetch(api).then((response) => {
+    return response.json();
+  });
+  return response;
+};
 
-    });
-    return response;
-    //   console.log(response);
-    //   console.log(response.location);
-    //   console.log(response.json);
-}
+let createIcon = (path) => {
+  let weatherIcon = document.getElementById("weatherIcon");
+  while (weatherIcon.firstChild) {
+    weatherIcon.removeChild(weatherIcon.firstChild);
+  }
+  let elem = document.createElement("img");
+  elem.setAttribute("src", path);
+  elem.setAttribute("alt", "Weather Icon");
+  document.getElementById("weatherIcon").appendChild(elem);
+};
+
 let setValue = () => {
-    console.log(setLocation.value);
-    let val=setLocation.value;
-    api="http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q="+val+"&aqi=no";   
-    // console.log(api);
-    let data = getWeatherData()
-    .then((result) => {
-        console.log(result);
-        place.innerText = result.location.name;
-        actualTemp.innerText = result.current.temp_f;
-        feelsLike.innerText = "Feels " + result.current.feelslike_f;
-        console.log(result.current.condition.text);
-        if (result.current.condition.text == "Overcast") {
-            weatherIcon.className += " fa-cloud ";
-        }
-        if (result.current.condition.text == "Partly cloudy") {
-            weatherIcon.className += " fa-cloud-sun ";
-        }
-        if (result.current.condition.text == "Sunny") {
-            weatherIcon.className += " fa-sun text-warning";
-        }
-    })
-}
+  console.log(setLocation.value);
+  let val = setLocation.value;
+  api =
+    "http://api.weatherapi.com/v1/current.json?key=0c80b2b56f1943ada19100744230103&q=" +
+    val +
+    "&aqi=no";
+  // console.log(api);
+  let data = getWeatherData().then((result) => {
+    console.log(result);
+    // place.innerText = result.location.name;
+    actualTemp.innerText = result.current.temp_f;
+    feelsLike.innerText = "Feels " + result.current.feelslike_f;
+    console.log(result.current.condition.text);
 
-// console.log(data);
-// console.log(data.PromiseResult);
+    let weather = result.current.condition.text;
 
-// place.innerText=data.location;
+    if (weather == "Mist") {
+      createIcon(
+        "./assets/images/amcharts_weather_icons_1.0.0/animated/cloudy-day-1.svg"
+      );
+    } else if (weather == "Partly cloudy" || weather == "Cloudy") {
+      createIcon(
+        "./assets/images/amcharts_weather_icons_1.0.0/animated/cloudy-day-3.svg"
+      );
+    } else if (weather == "Snowy") {
+      createIcon(
+        "./assets/images/amcharts_weather_icons_1.0.0/animated/snowy-2.svg"
+      );
+    } else if (weather == "Rainy" || weather == "Light rain") {
+      createIcon(
+        "./assets/images/amcharts_weather_icons_1.0.0/animated/rainy-7.svg"
+      );
+    } else if (weather == "Clear" || weather == "Sunny") {
+      createIcon(
+        "./assets/images/amcharts_weather_icons_1.0.0/animated/day.svg"
+      );
+    } else {
+      createIcon(
+        "./assets/images/amcharts_weather_icons_1.0.0/animated/weather.svg"
+      );
+    }
+  });
+};
+
+document.addEventListener('keyup', (e) => {
+    console.log(e);
+    if(e.key==="Enter"){
+        setValue();
+    }
+});
+
